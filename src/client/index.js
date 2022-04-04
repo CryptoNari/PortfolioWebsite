511d8d434b719431fdb6c5d5f7f5432dcc980cb9
Andreas Vieten <cryptonari@outlook.com>
2022-04-04 20:06:52 +0200

    FEAT:html structure, fonts, scss, icon-menu

--- src/client/index.js
+++ src/client/index.js
@@ -4,7 +4,24 @@ import printMe from './js/print.js';
 
 //import './logoipsum-logo-50.svg';
 
-function component() {
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelectorAll('.main-content');
+
+function pageTransition() {
+    //Button Click active class
+    for(let i = 0; i < sectBtn.length; i++){
+        sectBtn[i].addEventListener('click', function() {
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += ' active-btn';
+        })
+    }
+}
+
+
+/* function component() {
     const element = document.createElement('div');
     const btn = document.createElement('button');
 
@@ -14,17 +31,17 @@ function component() {
     btn.innerHTML = 'Click me and check the console!';
     btn.onclick = printMe;
 
-
-
-    // Add the image to our existing div.
-    /* const myIcon = new Image();
-    myIcon.src = Icon;
-
-    element.appendChild(myIcon); */
-
     element.appendChild(btn);
 
     return element;
 }
 
-document.body.appendChild(component());
+document.body.appendChild(component()); */
+
+pageTransition();
+
+// Add the image to our existing div.
+    /* const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon); */
