d9ee6f0885fbf920afafdc8d34dc9d8e907b6751
Andreas Vieten <cryptonari@outlook.com>
2022-04-05 21:06:56 +0200

    FEAT: display sections by control/menu

--- src/client/index.js
+++ src/client/index.js
@@ -10,14 +10,19 @@ const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelectorAll('.main-content');
 
 function pageTransition() {
-    //Button Click active class
+    //Button Click active class/section
     for(let i = 0; i < sectBtn.length; i++){
         sectBtn[i].addEventListener('click', function() {
             let currentBtn = document.querySelectorAll('.active-btn');
-            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            let currentSec = document.querySelectorAll('.active');
+            currentBtn[0].className = currentBtn[0].className.replace(' active-btn', '');
+            currentSec[0].className = currentSec[0].className.replace(' active', '');
             this.className += ' active-btn';
+            sections[i].className += ' active';
         })
     }
+
+
 }
 
 
