6f48e301f7c09398f950d219d4a343a483d8e89a
Andreas Vieten <cryptonari@outlook.com>
2022-04-09 17:55:53 +0200

    feat:portfolio section draft

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,12 @@
 import _ from 'lodash';
 import './styles/style.scss';
 import homeImage from './img/home.jpg';
+import portImage1 from './img/port1.jpg'
+import portImage2 from './img/port2.jpg';
+import portImage3 from './img/port3.jpg';
+import portImage4 from './img/port4.jpg';
+import portImage5 from './img/port5.jpg';
+import portImage6 from './img/port6.jpg';
 import printMe from './js/print.js';
 
 //import './logoipsum-logo-50.svg';
@@ -27,7 +33,20 @@ function pageTransition() {
 }
 
 var homeImg = document.getElementById('img-home');
+var portImg1 = document.getElementById('port-img-1');
+var portImg2 = document.getElementById('port-img-2');
+var portImg3 = document.getElementById('port-img-3');
+var portImg4 = document.getElementById('port-img-4');
+var portImg5 = document.getElementById('port-img-5');
+var portImg6 = document.getElementById('port-img-6');
+
 homeImg.src = homeImage;
+portImg1.src = portImage1;
+portImg2.src = portImage2;
+portImg3.src = portImage3;
+portImg4.src = portImage4;
+portImg5.src = portImage5;
+portImg6.src = portImage6;
 
 /* function component() {
     const element = document.createElement('div');
