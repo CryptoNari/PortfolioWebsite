30715c4476e9f702277303051e8eed97566b348a
Andreas Vieten <cryptonari@outlook.com>
2022-04-12 17:03:00 +0200

    feat:light/dark theme toggle mode

--- src/client/index.js
+++ src/client/index.js
@@ -29,6 +29,13 @@ function pageTransition() {
         })
     }
 
+    // Toggle Dark/light Theme
+    const themeBtn = document.querySelector('.theme-btn');
+
+    themeBtn.addEventListener('click', () => {
+        let element = document.body;
+        element.classList.toggle('light-mode')
+    })
 
 }
 
